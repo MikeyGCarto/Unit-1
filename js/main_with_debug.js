4c37d6a0b12283253e3147cb85feed507116cf2d
MikeyGCarto <117098956+MikeyGCarto@users.noreply.github.com>
2024-02-07 10:33:01 -0600

    Update main_with_debug.js

--- js/main_with_debug.js
+++ js/main_with_debug.js
@@ -1,185 +1,77 @@
-function initialize(){
-    cities();
-};
-
-function cities(){
-    var cities = [
-        'Madison',
-        'Milwaukee',
-        'Green Bay',
-        'Superior'
-    ];
-    var population = [
-        233209,
-        594833,
-        104057,
-        27244
-    ];
-
-    var table = document.createElement("table");
-
-    var headerRow = document.createElement("tr");
-
-    var cityHeader = document.createElement("th");
-    cityHeader.innerHTML = "City";
-    headerRow.appendChild(cityHeader);
-
-    var popHeader = document.createElement("th");
-    popHeader.innerHTML = "Population";
-    headerRow.appendChild(popHeader);
-
-    table.appendChild(headerRow);
-
-    for (var i = 0; i < cities.length; i++){
-        var tr = document.createElement("tr");
-
-        var city = document.createElement("td");
-        city.innerHTML = cities[i];
-        tr.appendChild(city);
-
-        var pop = document.createElement("td");
-        pop.innerHTML = population[i];
-        tr.appendChild(pop);
-
-        table.appendChild(tr);
-    };
-
-    var mydiv = document.getElementById("mydiv");
-    mydiv.appendChild(table);
-};
-
-window.onload = initialize();
-
-
-
-
 var cityPop = [
-	{ 
-		city: 'Madison',
-		population: 233209
-	},
-	{
-		city: 'Milwaukee',
-		population: 594833
-	},
-	{
-		city: 'Green Bay',
-		population: 104057
-	},
-	{
-		city: 'Superior',
-		population: 27244
-	}
+    { 
+        city: 'Madison',
+        population: 233209
+    },
+    {
+        city: 'Milwaukee',
+        population: 594833
+    },
+    {
+        city: 'Green Bay',
+        population: 104057
+    },
+    {
+        city: 'Superior',
+        population: 27244
+    }
 ];
 
 function addColumns(cityPop){
-    
-    document.querySelectorAll("tr").forEach(function(row, i){
+    // Loop through each row (tr) in the table
+    $("tr").each(function(i, row){
 
-    	if (i == 0){
+        if (i == 0){
+            // Add a header for "City Size" to the first row
+            $(row).append('<th>City Size</th>');
+        } else {
+            var citySize;
 
-    		row.insertAdjacntHTML('beforeend', '<th>City Size</th>');
-    	} else {
+            // Determine the city size based on population
+            if (cityPop[i-1].population < 100000){
+                citySize = 'Small';
+            } else if (cityPop[i-1].population < 500000){
+                citySize = 'Medium';
+            } else {
+                citySize = 'Large';
+            }
 
-    		var citySize;
-
-    		if (cityPop[i-1].population < 100000){
-    			citySize = 'Small';
-
-    		} else if (cityPop[i-1].population < 500000){
-    			citysize = 'Medium';
-
-    		} else {
-    			citySize = 'Large';
-    		};
-
-			row.insertAdjacntHTML = '<td' + citySize + '</td>';
-    	};
+            // Add a cell with the city size to the current row
+            $(row).append('<td>' + citySize + '</td>');
+        }
     });
-};
+}
 
 function addEvents(){
+    // Add mouseover event to the table
+    $("table").mouseover(function(){
+        var color = "rgb(";
 
-	document.querySelector("table").addEventListener("mouseover", function(){
-		
-		var color = "rgb(";
+        // Generate random RGB color
+        for (var i=0; i<3; i++){
+            var random = Math.round(Math.random() * 255);
+            color += random;
+            if (i<2){
+                color += ",";
+            } else {
+                color += ")";
+            }
+        }
 
-		for (var i=0; i<3; i++){
-
-			var random = Math.round(Math.random() * 255);
-
-			color += "random";
-
-			if (i<2){
-				color += ",";
-			
-			} else {
-				color += ")";
-		};
-
-		document.querySelector("table").color = color;
-	});
-
-	function clickme(){
-
-		alert('Hey, you clicked me!');
-	};
-
-	document.querySelector("table").addEventListener("click", clickme)
-};
-
-function addColumns(cityPop){
-    
-    $('tr').each(function(i){
-
-    	if (i == 0){
-
-    		$(this).apend('<th>City Size</th>');
-    	} else {
-
-    		var citySize;
-
-    		if (cityPop[i-1].population < 100000){
-    			citySize = 'Small';
-
-    		} else if (cityPop[i-1].population < 500000){
-    			citysize = 'Medium';
-
-    		} else {
-    			citySize = 'Large';
-    		};
-
-    		$this.append('<td' + citySize + '</td>');
-    	};
+        // Apply the generated color to the table's text color
+        $(this).css('color', color);
     });
-};
 
-function addEvents(){
+    // Add click event to the table
+    $("table").on('click', function(){
+        alert('Hey, you clicked me!');
+    });
+}
 
-	$('#table').mouseover(function(){
-		
-		var color = "rgb(";
-
-		for (var i=0; i<3; i++){
-
-			var random = Math.round(Math.random() * 255);
-
-			color += "random";
-
-			if (i<2){
-				color += ",";
-			
-			} else {
-				color += ")";
-		};
-
-		$(this).css('color', color);
-	});
-
-	function clickme(){
-
-		alert('Hey, you clicked me!');
-	};
-
-	$('table').on('click', clickme);
-};
+// Initialize the table and events when the document is ready
+$(document).ready(function(){
+    // Call the function to add columns based on city population
+    addColumns(cityPop);
+    // Call the function to add hover and click events to the table
+    addEvents();
+});
