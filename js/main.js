3a440cca29640f04e1cc186566749f73e61a1145
MikeyGCarto <117098956+MikeyGCarto@users.noreply.github.com>
2024-02-07 09:48:35 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 // Mikey G
-console.log("main.js")
+// JavaScript code to interact with index.html
+
+// Example: Add a click event listener to a button with id "myButton"
+document.getElementById("myButton").addEventListener("click", function() {
+    alert("Button clicked!");
+});
+
+// Example: Manipulate the content of an HTML element with id "myDiv"
+document.getElementById("myDiv").innerHTML = "New content added by main.js";
