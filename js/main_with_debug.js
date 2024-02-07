90c653c844534949018ed887abc61941cc91b6ed
MikeyGCarto <117098956+MikeyGCarto@users.noreply.github.com>
2024-02-07 09:49:13 -0600

    Update main_with_debug.js

--- js/main_with_debug.js
+++ js/main_with_debug.js
@@ -1,3 +1,14 @@
+// JavaScript code to interact with index.html
+
+// Example: Add a click event listener to a button with id "myButton"
+document.getElementById("myButton").addEventListener("click", function() {
+    alert("Button clicked!");
+});
+
+// Example: Manipulate the content of an HTML element with id "myDiv"
+document.getElementById("myDiv").innerHTML = "New content added by main.js";
+
+
 var cityPop = [
     { 
         city: 'Madison',
