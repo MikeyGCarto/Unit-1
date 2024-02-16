6b5bfc16fedf0e08a7fc0d3b0f65c92013002152
MikeyGCarto <117098956+MikeyGCarto@users.noreply.github.com>
2024-02-15 19:34:51 -0600

    Update main_with_debug.js

--- Chapter 2/js/main_with_debug.js
+++ Chapter 2/js/main_with_debug.js
@@ -142,21 +142,24 @@ function debugCallback(response) {
         document.querySelector("#mydiv").insertAdjacentHTML('beforeend', 'GeoJSON data: ' + JSON.stringify(myData));
     });
 }
+
 function debugAjax() {
     var myData;
 
-    fetch("Chapter 2/data/MegaCities.csv")
+    fetch("data/MegaCities.geojson")
         .then(function(response) {
-            // Parse the response as text instead of JSON
-            return response.text();
+            return response.json();
         })
         .then(function(data) {
-            // Process the CSV data as needed
-            // For example, you can split the data by lines or commas
             myData = data;
-            document.querySelector("#mydiv").insertAdjacentHTML('beforeend', '<br>CSV data:<br>' + myData);
+            document.querySelector("#mydiv").insertAdjacentHTML('beforeend', '<br>GeoJSON data:<br>' + JSON.stringify(myData));
         })
         .catch(function(error) {
             console.error('Error fetching data:', error);
         });
+
+    document.querySelector("#mydiv").insertAdjacentHTML('beforeend', 'GeoJSON data: ' + JSON.stringify(myData));
 }
+
+debugAjax();
+
